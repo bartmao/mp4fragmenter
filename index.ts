b0aa12f945d3a3ee2147901e76cae2198ab62b57
bartmao <maochen.mars@qq.com>
2017-06-18 22:54:03 +0800

    code update

--- index.ts
+++ index.ts
@@ -11,71 +11,108 @@ class Sample {
     Description: number;
 }
 
-class Context {
+class TrackContext {
+    Type: string;
+    Timescale: number;
     ChunkOffset: Array<number> = [];
     SampleToChunk: Array<{ FirstChunk: number, SamplesPerChunk: number, SampleDescriptionIndex: number }> = [];
     SampleSize: Array<number> = [];
     Dt: Array<{ SampleCount: number, SampleDelta: number }> = [];
     Ct: Array<{ SampleCount: number, SampleOffset: number }> = [];
     SyncSample: Array<number> = [];
+}
 
-    public getSamples(): Array<Sample> {
-        let samples = [];
+class MoovContext {
+    Tracks: Array<TrackContext> = [];
+    CurrentTrack: TrackContext;
+
+    public getSamples(trackId: number): Array<Sample> {
+        let samples: Array<Sample> = [];
+
+        let track = this.Tracks[trackId];
         let i_SampleToChunk = 0;
-        let c_SampleToChunk = this.SampleToChunk[i_SampleToChunk].SamplesPerChunk;
-        let chunk = this.SampleToChunk[i_SampleToChunk].FirstChunk;
-        let curSampleOffset = this.ChunkOffset[chunk - 1];
+        let c_SampleToChunk = track.SampleToChunk[i_SampleToChunk].SamplesPerChunk;
+        let chunk = track.SampleToChunk[i_SampleToChunk].FirstChunk;
+        let curSampleOffset = track.ChunkOffset[chunk - 1];
         let i_dt = 0;
-        let c_dt = this.Dt[i_dt].SampleCount;
+        let c_dt = track.Dt[i_dt].SampleCount;
         let i_ct = 0;
-        let c_ct = this.Ct[i_ct].SampleCount;
+        let c_ct = track.Ct[i_ct].SampleCount;
         let curSampleDt = 0;
         let i_SyncSample = 0;
-        for (var i = 0; i < this.SampleSize.length; i++) {
+        for (var i = 0; i < track.SampleSize.length; i++) {
             var sample = new Sample();
-            sample.Size = this.SampleSize[i];
+            sample.Size = track.SampleSize[i];
             if (c_SampleToChunk == 0) {
-                c_SampleToChunk = this.SampleToChunk[++i_SampleToChunk].SamplesPerChunk;
+                c_SampleToChunk = track.SampleToChunk[++i_SampleToChunk].SamplesPerChunk;
                 chunk++;
-                curSampleOffset = this.ChunkOffset[chunk - 1];
+                curSampleOffset = track.ChunkOffset[chunk - 1];
             }
             c_SampleToChunk--;
             sample.Chunk = chunk;
-            sample.Description = this.SampleToChunk[i_SampleToChunk].SampleDescriptionIndex;
+            sample.Description = track.SampleToChunk[i_SampleToChunk].SampleDescriptionIndex;
             sample.Position = curSampleOffset;
             curSampleOffset += sample.Size;
 
             if (c_dt == 0) {
-                c_dt = this.Dt[++i_dt].SampleCount;
+                c_dt = track.Dt[++i_dt].SampleCount;
             }
             c_dt--;
             if (c_ct == 0) {
-                c_ct = this.Ct[++i_ct].SampleCount;
+                c_ct = track.Ct[++i_ct].SampleCount;
             }
             c_ct--;
             sample.DT = curSampleDt;
-            curSampleDt += this.Dt[i_dt].SampleDelta;
-            sample.CT = sample.DT + this.Ct[i_ct].SampleOffset;
+            curSampleDt += track.Dt[i_dt].SampleDelta;
+            sample.CT = sample.DT + track.Ct[i_ct].SampleOffset;
 
-            if (i + 1 == this.SyncSample[i_SyncSample]) {
+            if (i + 1 == track.SyncSample[i_SyncSample]) {
                 sample.Type = 'I';
-                if (i_SyncSample < this.SyncSample.length)
+                if (i_SyncSample < track.SyncSample.length)
                     i_SyncSample++;
             }
-            // else if (sample.DT < sample.CT) {
-            //     sample.Type = 'P';
-            // }
-            // else {
-            //     sample.Type = 'B';
-            // }
             samples.push(sample);
         }
+
         return samples;
+    }
+
+    private distinct(arr: Array<any>) {
+        var flags = [], output = [], l = arr.length, i;
+        for (i = 0; i < l; i++) {
+            if (flags[arr[i]]) continue;
+            flags[arr[i]] = true;
+            output.push(arr[i]);
+        }
+        return output;
+    }
+
+    public split(from: number, to: number) {
+        var track = this.Tracks[0];
+        var samples = this.getSamples(0);
+        var timescale = track.Timescale;
+        var filtered = samples.filter(s => s.DT >= from * timescale && s.DT <= to * timescale);
+
+        var keptDescriptions = this.distinct(filtered.map(s => s.Description));
+        var keptChunks = this.distinct(filtered.map(s => s.Chunk));
+
+        let i = 0;
+        let new_stts = [];
+        let delta = 0;
+        filtered.forEach(s => {
+            if (new_stts.length == 0) {
+                new_stts.push({ sample_count: 1, sample_delta: filtered[1].DT });
+                delta = filtered[i].DT;
+            }else{
+                ///let newDelta = s.DT - delta;
+                //if(new_stts[i].count = )
+            }
+        });
     }
 }
 
 let parser = new MP4Parser(fs.createReadStream('resource/30s.mp4'));
-let context = new Context();
+let context = new MoovContext();
 parser.on('atom', atom => {
     var seq = "00" + atom._seq;
     seq = seq.substring(seq.length - 3, seq.length);
@@ -83,6 +120,19 @@ parser.on('atom', atom => {
 });
 
 function init() {
+    parser.on('data_hdlr', data => {
+        let atom = data as Buffer;
+        context.CurrentTrack.Type = atom.toString('utf8', 12 + 4, 4);
+    });
+
+    parser.on('data_mdhd', data => {
+        let atom = data as Buffer;
+        var track = new TrackContext();
+        context.Tracks.push(track);
+        context.CurrentTrack = track;
+        context.CurrentTrack.Timescale = atom.readUInt32BE(12 + 8);
+    });
+
     parser.on('data_stco', data => {
         let atom = data as Buffer;
         let offset = 12;
@@ -92,7 +142,7 @@ function init() {
             let chunk_offset = atom.readUInt32BE(offset);
             offset += 4;
             //console.log(`chunk_offset ${chunk_offset}`);
-            context.ChunkOffset.push(chunk_offset);
+            context.CurrentTrack.ChunkOffset.push(chunk_offset);
         }
     });
     parser.on('data_stsc', data => {
@@ -110,7 +160,7 @@ function init() {
             // console.log(`first_chunk ${first_chunk}`);
             // console.log(`samples_per_chunk ${samples_per_chunk}`);
             // console.log(`sample_description_index ${sample_description_index}`);
-            context.SampleToChunk.push({
+            context.CurrentTrack.SampleToChunk.push({
                 FirstChunk: first_chunk,
                 SamplesPerChunk: samples_per_chunk,
                 SampleDescriptionIndex: sample_description_index
@@ -129,7 +179,7 @@ function init() {
             for (var i = 0; i < sample_count; i++) {
                 let entry_size = atom.readUInt32BE(offset);
                 offset += 4;
-                context.SampleSize.push(entry_size);
+                context.CurrentTrack.SampleSize.push(entry_size);
             }
     });
     parser.on('data_stts', data => {
@@ -142,7 +192,7 @@ function init() {
             offset += 4;
             let sample_delta = atom.readUInt32BE(offset);
             offset += 4;
-            context.Dt.push({
+            context.CurrentTrack.Dt.push({
                 SampleCount: sample_count,
                 SampleDelta: sample_delta
             });
@@ -160,7 +210,7 @@ function init() {
             offset += 4;
             let sample_offset = atom.readUInt32BE(offset);
             offset += 4;
-            context.Ct.push({
+            context.CurrentTrack.Ct.push({
                 SampleCount: sample_count,
                 SampleOffset: sample_offset
             });
@@ -176,7 +226,7 @@ function init() {
         for (var i = 0; i < entry_count; i++) {
             let sample_number = atom.readUInt32BE(offset);
             offset += 4;
-            context.SyncSample.push(sample_number);
+            context.CurrentTrack.SyncSample.push(sample_number);
             //console.log(`sample_number ${sample_number}`);
         }
     });
@@ -186,14 +236,15 @@ init();
 parser.start();
 
 setTimeout(function () {
-    var samples = context.getSamples();
-    var counter = 1;
-    samples.forEach(s => {
-        console.log(`${counter++}:${s.Position}, ${s.Size}, ${s.DT}, ${s.CT},${s.Chunk}`)
-    });
-    //console.log(samples);
+    context.split(0, 100);
+    // var samples = context.getSamples();
+    // var counter = 1;
+    // samples.forEach(s => {
+    //     console.log(`${counter++}:${s.Position}, ${s.Size}, ${s.DT}, ${s.CT},${s.Chunk}`)
+    // });
+    // //console.log(samples);
 }, 2000);
 
-function splitVideo(start:number, end:number){
-    
+function splitVideo(start: number, end: number) {
+
 }
